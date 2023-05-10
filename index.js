5391fe89e2d4acbdc9023ef211edf3c7219c0e2f
gadnadj <gadnadjar@gmail.com>
2023-05-10 19:33:42 +0300

    add tests

--- index.js
+++ index.js
@@ -1,46 +1,14 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const { Student } = require('./db');
 
 const app = express();
 
 // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 app.use(express.json());
 
-// Servez vos API
-app.get('/api', (req, res) => {
-  res.send('Hello from API');
-});
-
-app.get('/api/test', (req, res) => {
-  res.send('Test Hello from Gad Nadjar');
-});
-
 // Servez vos fichiers statiques du front-end
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Enregistrer un nouvel étudiant
-app.post('/api/students', async (req, res) => {
-  try {
-    const { name, grade1, grade2, grade3 } = req.body;
-    const student = new Student({ name, grade1, grade2, grade3 });
-    await student.save();
-    res.status(201).json(student);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-// Obtenir tous les étudiants
-app.get('/api/students', async (req, res) => {
-  try {
-    const students = await Student.find();
-    res.json(students);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
 
 // Connexion à la base de données
 mongoose
--- tests/test1.test.js
+++ tests/test1.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest');
 const app = require('../server');
+const server = require('../server');
+
+afterAll((done) => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      done(err);
+    } else {
+      done();
+    }
+  });
+}, 20000);
 
 describe('Test suite 1:', () => {
   test('test 1:', async () => {
@@ -11,9 +23,24 @@ describe('Test suite 1:', () => {
     const res = await request(app).get('/132413');
     expect(res.statusCode).toEqual(404);
   });
+});
 
-  afterAll(async () => {
-    // Fermer le serveur après la fin de tous les tests
-    await app.close();
+describe('GET /api', () => {
+  it('should respond with "Hello from API"', async () => {
+    const res = await request(server).get('/api');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual('Hello from API');
+  });
+});
+
+describe('POST /api/students', () => {
+  it('should reject invalid student data', async () => {
+    const res = await request(server).post('/api/students').send({
+      name: 'John',
+      grade1: -10, // Invalid grade
+      grade2: 120, // Invalid grade
+      grade3: 'abc', // Invalid grade
+    });
+    expect(res.statusCode).toEqual(400);
   });
 });
